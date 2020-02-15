8cdb14945591f948dac826986c5ff1465c9554fe
Matt Corallo <git@bluematt.me>
2020-02-14 22:51:09 -0500

    Don't send padding bytes for pings

--- lightning/src/ln/peer_handler.rs
+++ lightning/src/ln/peer_handler.rs
@@ -1112,7 +1112,7 @@ impl<Descriptor: SocketDescriptor, CM: Deref> PeerManager<Descriptor, CM> where 
 
 				let ping = msgs::Ping {
 					ponglen: 0,
-					byteslen: 64,
+					byteslen: 0,
 				};
 				peer.pending_outbound_buffer.push_back(encode_msg!(&ping));
 				let mut descriptor_clone = descriptor.clone();
