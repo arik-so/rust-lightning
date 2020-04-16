0c2cc74f5753374d2a4fe3b716d9d75ddbfc5304
Arik Sosman <git@arik.io>
2020-04-16 15:38:13 -0700

    add wasm component for handshake

--- bindings/src/peers/handshake.rs
+++ bindings/src/peers/handshake.rs
@@ -8,7 +8,72 @@ use crate::buffer::{BufferArgument, BufferResponse};
 use crate::error::Error;
 use crate::peers::conduit::Conduit;
 
+#[wasm_bindgen]
 pub struct PeerHandshake(RawHandshake);
+
+impl PeerHandshake{
+
+	#[wasm_bindgen]
+	pub fn new_outbound(private_key_slice: &[u8], ephemeral_private_key_slice: &[u8], public_key_slice: &[u8]) -> Self {
+		let private_key_object = SecretKey::from_slice(private_key_slice).unwrap();
+		let ephemeral_private_key_object = SecretKey::from_slice(ephemeral_private_key_slice).unwrap();
+		let public_key_object = PublicKey::from_slice(public_key_slice).unwrap();
+
+		let handshake = RawHandshake::new_outbound(&private_key_object, &public_key_object, &ephemeral_private_key_object);
+		let peer_handshake = Self(handshake);
+		peer_handshake
+	}
+
+	#[wasm_bindgen]
+	pub fn new_inbound(private_key_slice: &[u8], ephemeral_private_key_slice: &[u8]) -> Self {
+		let private_key_object = SecretKey::from_slice(private_key_slice).unwrap();
+		let ephemeral_private_key_object = SecretKey::from_slice(ephemeral_private_key_slice).unwrap();
+
+		let handshake = RawHandshake::new_inbound(&private_key_object, &ephemeral_private_key_object);
+		let peer_handshake = Self(handshake);
+		peer_handshake
+	}
+
+	#[wasm_bindgen]
+	pub fn process_act(&mut self, input_data: &[u8], ) -> WasmHandshakeResult {
+		let response = peer.0.process_act(input_data);
+
+		/*if response.is_err() {
+			// handle error
+			let ffi_error: Error = response.err().unwrap().into();
+			unsafe { std::ptr::write(error, ffi_error); }
+			return std::ptr::null_mut();
+		}*/
+
+		let act_response = response.unwrap();
+		let mut result = WasmHandshakeResult {
+			next_act: None,
+			conduit: None,
+		};
+
+		let next_act_option = act_response.0;
+		if let Some(next_act) = next_act_option {
+			let next_act_vector = next_act.serialize();
+			println!("next_act_vector: {:?}", next_act_vector);
+			result.next_act = Some(next_act_vector);
+		}
+
+		let conduit_option = act_response.1;
+		if let Some(conduit) = conduit_option {
+			let wrapped_conduit = Conduit(conduit);
+			result.conduit = Some(wrapped_conduit);
+		}
+
+		result
+	}
+
+}
+
+#[wasm_bindgen]
+pub struct WasmHandshakeResult {
+	pub next_act: Option<Vec<u8>>,
+	pub conduit: Option<Conduit>,
+}
 
 #[repr(C)]
 pub struct HandshakeResult {
@@ -17,6 +82,7 @@ pub struct HandshakeResult {
 }
 
 #[no_mangle]
+#[wasm_bindgen]
 pub extern "C" fn peer_handshake_new_outbound(private_key: *const u8, ephemeral_private_key: *const u8, remote_public_key: *const u8) -> *mut PeerHandshake {
 	let private_key_slice = unsafe {
 		assert!(!private_key.is_null());
@@ -46,6 +112,7 @@ pub extern "C" fn peer_handshake_new_outbound(private_key: *const u8, ephemeral_
 }
 
 #[no_mangle]
+#[wasm_bindgen]
 pub extern "C" fn peer_handshake_new_inbound(private_key: *const u8, ephemeral_private_key: *const u8) -> *mut PeerHandshake {
 	let private_key_slice = unsafe {
 		assert!(!private_key.is_null());
@@ -69,6 +136,7 @@ pub extern "C" fn peer_handshake_new_inbound(private_key: *const u8, ephemeral_p
 }
 
 #[no_mangle]
+#[wasm_bindgen]
 pub extern "C" fn peer_handshake_process_act(peer: &mut PeerHandshake, act_data: &BufferArgument, error: *mut Error) -> *mut HandshakeResult {
 	let input_data = unsafe { act_data.to_vec() };
 
