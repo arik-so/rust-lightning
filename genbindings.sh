936e72112798b6c0d85a80ecdf4b862a052fd6d5
Matt Corallo <git@bluematt.me>
2020-05-12 14:54:12 -0400

    Add all the manually-generated bits for the c-bindings crate
    Including:
     * A script to automatically generate all the rest,
     * Cargo.toml and cbindgen.toml,
     * manually-written wrapper types for a few types

---
+++ genbindings.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+cd c-bindings-gen && cargo build && cd ..
+GEN="$(pwd)/c-bindings-gen/target/debug/c-bindings-gen"
+SRC="$(pwd)/lightning/src"
+OUT="$(pwd)/lightning-c-bindings/src"
+OUT_F="$(pwd)/lightning-c-bindings/include/rust_types.h"
+echo > $OUT_F
+
+rm lightning-c-bindings/src/{ln,util,chain,routing}/*
+RUST_BACKTRACE=1 $GEN $SRC/ $OUT/ lightning "" $OUT_F
+
+cd lightning-c-bindings
+cargo build
+PATH="$PATH:~/.cargo/bin"
+cbindgen -v --config cbindgen.toml -o include/lightning.h
+gcc -static -pthread demo.c ../target/debug/liblightning.a -ldl
+echo "Bin size w/o optimization:"
+ls -lha a.out
+
+cargo rustc -v --release -- -C lto
+clang -flto -O2 -static -pthread demo.c ../target/release/liblightning.a -ldl
+echo "Bin size with only RL optimized:"
+ls -lha a.out
+
+cargo rustc -v --release -- -C linker-plugin-lto -C lto
+clang -flto -O2 -static -pthread demo.c ../target/release/liblightning.a -ldl
+echo "Bin size with cross-language LTO:"
+ls -lha a.out
---
+++ lightning-c-bindings/src/c_types/mod.rs
@@ -0,0 +1,73 @@
+use bitcoin::Script as BitcoinScript;
+use bitcoin::secp256k1::key::PublicKey as SecpPublicKey;
+use bitcoin::secp256k1::key::SecretKey as SecpSecretKey;
+
+#[repr(C)]
+pub struct PublicKey {
+	pub compressed_form: [u8; 33],
+}
+impl PublicKey {
+	pub(crate) fn from_rust(pk: &SecpPublicKey) -> Self {
+		Self {
+			compressed_form: pk.serialize(),
+		}
+	}
+	pub(crate) fn into_rust(&self) -> SecpPublicKey {
+		SecpPublicKey::from_slice(&self.compressed_form).unwrap()
+	}
+}
+
+#[repr(C)]
+pub struct SecretKey {
+	pub bytes: [u8; 32],
+}
+impl SecretKey {
+	// from_rust isn't implemented since we jsut return byte array refs directly
+	pub(crate) fn into_rust(&self) -> SecpSecretKey {
+		SecpSecretKey::from_slice(&self.bytes).unwrap()
+	}
+}
+
+#[repr(C)]
+pub struct Script {
+	pub data: *const u8,
+	pub datalen: usize
+}
+impl Script {
+	pub(crate) fn into_bitcoin(&self) -> BitcoinScript {
+		BitcoinScript::from(unsafe { std::slice::from_raw_parts(self.data, self.datalen) }.to_vec())
+	}
+}
+
+#[repr(C)]
+pub struct Transaction {
+	pub data: *const u8,
+	pub datalen: usize
+}
+impl Transaction {
+	pub(crate) fn from_slice(s: &[u8]) -> Self {
+		Self {
+			data: s.as_ptr(),
+			datalen: s.len(),
+		}
+	}
+}
+
+#[repr(C)]
+pub struct u8slice {
+	pub data: *const u8,
+	pub datalen: usize
+}
+impl u8slice {
+	pub(crate) fn from_slice(s: &[u8]) -> Self {
+		Self {
+			data: s.as_ptr(),
+			datalen: s.len(),
+		}
+	}
+}
+
+#[repr(C)]
+pub struct ThirtyTwoBytes {
+	pub data: [u8; 32],
+}
